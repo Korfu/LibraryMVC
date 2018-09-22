e300cabeb224a3c6e23140cb80b9a858e752228c
korfu92 <konrad.korf@gmail.com>
2018-09-22 11:42:53 +0200

    first commit

---
+++ Library.Repositories/BooksRepository.cs
@@ -0,0 +1,42 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Library.Repositories
+{
+    public class BooksRepository
+    {
+        private static List<Book> _allBooks = new List<Book>
+        {
+            new Book
+            {
+                Id=1,
+                Title="Myśli współczesnego człowieka",
+                Author="Roma Dmowski",
+                ProductionYear = 1920,
+                Genre = new Genre
+                {
+                    Id =1,
+                    Name ="Political fiction"
+                }
+            },
+            new Book
+            {
+                Id=1,
+                Title="Czerwona książeczka",
+                Author="Mao Ze Tung",
+                ProductionYear = 1925,
+                Genre = new Genre
+                {
+                    Id =1,
+                    Name ="Political fiction"
+                }
+            }
+        };
+
+        public List<Book> GetBooks()
+        {
+            return _allBooks;
+        }
+    }
+}
