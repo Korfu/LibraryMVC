41ddcb1d0957cf752da0fd76a0acb241fad8fd29
korfu92 <konrad.korf@gmail.com>
2018-09-23 11:57:14 +0200

    FluentValidation added

---
+++ Library.Models/BookValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Models
+{
+    public class BookValidator : AbstractValidator<Book>
+    {
+        public BookValidator()
+        {
+            RuleFor(x => x.Author).NotNull();
+            RuleFor(x => x.Title).Length(1, 50).NotNull();
+            RuleFor(x => x.ProductionYear).ExclusiveBetween(1, DateTime.Now.Year);
+        }
+    }
+}
